250c7242559c43704243b901b41ea374874b7049
actboy168 <actboy168@gmail.com>
2018-05-14 11:32:55 +0800

    0.1.0

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # vscode-tasks
+This extension loads VSCode tasks into status bar.
+
+## Source
+
+[GitHub](https://github.com/actboy168/vscode-tasks)
+
+## License
+
+[MIT](https://raw.githubusercontent.com/actboy168/vscode-tasks/master/LICENSE)
---
+++ js/extension.js
@@ -0,0 +1,47 @@
+const vscode = require('vscode');
+
+var statusBarArray = [];
+var taskMap = {};
+
+function activate(context) {
+    context.subscriptions.push(vscode.workspace.onDidChangeConfiguration(() => {
+        loadTasks(context);
+    }));
+    statusBarArray = [];
+    taskMap = {};
+    loadTasks(context);
+}
+exports.activate = activate;
+
+function deactivate() {
+}
+exports.deactivate = deactivate;
+
+function loadTasks(context) {
+    statusBarArray.forEach(i => {
+        i.hide();
+    });
+    statusBarArray = [];
+    const config = vscode.workspace.getConfiguration('tasks');
+    if (!config || !Array.isArray(config.tasks)) {
+        return;
+    }
+    for (const task of config.tasks) {
+        const name = "label" in task ? task.label : task.taskName;
+        if (typeof name != 'string') {
+            continue;
+        }
+        let statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+        statusBar.text = name;
+        statusBar.command = "actboy168.task-" + name;
+        statusBar.show();
+        statusBarArray.push(statusBar);
+        context.subscriptions.push(statusBar);
+        if (!(name in taskMap)) {
+            context.subscriptions.push(vscode.commands.registerCommand(statusBar.command, () => {
+                vscode.commands.executeCommand("workbench.action.tasks.runTask", name);
+            }));
+        }
+        taskMap[name] = true
+    }
+}
