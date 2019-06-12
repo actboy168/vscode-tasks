dfde4aa48e22ae3333ff27eb08f8137c5a14d525
actboy168 <actboy168@gmail.com>
2019-06-12 10:57:43 +0800

    使用task API重写

--- js/extension.js
+++ js/extension.js
@@ -1,52 +1,84 @@
 const vscode = require('vscode');
 
 var statusBarArray = [];
-var taskMap = {};
+var statusBarIndex = 0;
+var commandMap = {};
 
-function deinitTasks(context) {
+function deactivate(context) {
     statusBarArray.forEach(i => {
         i.dispose();
     });
     statusBarArray = [];
+    statusBarIndex = 0;
+}
+
+function computeTaskExecutionId(values) {
+	let id = '';
+	for (let i = 0; i < values.length; i++) {
+		id += values[i].replace(/,/g, ',,') + ',';
+	}
+	return id;
+}
+
+function computeId(task) {
+    const props = [];
+    props.push(task.type);
+    if (task.command !== undefined) {
+        props.push(task.command);
+    }
+    if (task.args && task.args.length > 0) {
+        for (let arg of task.args) {
+            props.push(arg);
+        }
+    }
+    return computeTaskExecutionId(props);
 }
 
 function loadTasks(context) {
-    deinitTasks(context)
-    if (vscode.workspace.workspaceFolders == undefined) {
+    deactivate(context)
+    if (vscode.workspace.workspaceFolders === undefined) {
         return;
     }
+
+    let hide = {}
     for (const workspaceFolder of vscode.workspace.workspaceFolders) {
         const config = vscode.workspace.getConfiguration('tasks', workspaceFolder.uri);
         if (!config || !Array.isArray(config.tasks)) {
             continue;
         }
         for (const task of config.tasks) {
-            const name = "label" in task ? task.label : task.taskName;
-            if (typeof name != 'string') {
-                continue;
-            }
             if (task.options && task.options.statusbar == 'hide') {
+                let taskId = computeId(task);
+                hide[taskId] = true;
+            }
+        }
+    }
+
+    vscode.tasks.fetchTasks().then((tasks)=>{
+        for (const task of tasks) {
+            let name = task.name;
+            let taskId = task._definition.id;
+            if (hide[taskId]) {
                 continue;
             }
             let statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 51);
+            let command = "actboy168.task." + statusBarIndex++;
             statusBar.text = name;
-            statusBar.command = "actboy168.task-" + name;
+            statusBar.command = command;
             statusBar.show();
             statusBarArray.push(statusBar);
             context.subscriptions.push(statusBar);
-            if (!(name in taskMap)) {
-                context.subscriptions.push(vscode.commands.registerCommand(statusBar.command, () => {
-                    vscode.commands.executeCommand("workbench.action.tasks.runTask", name);
+            if (!(command in commandMap)) {
+                context.subscriptions.push(vscode.commands.registerCommand(command, () => {
+                    vscode.tasks.executeTask(commandMap[command]);
                 }));
             }
-            taskMap[name] = true
+            commandMap[command] = task;
         }
-    }
+    });
 }
 
-function initTasks(context) {
-    statusBarArray = [];
-    taskMap = {};
+function activate(context) {
     context.subscriptions.push(vscode.workspace.onDidChangeConfiguration(() => {
         loadTasks(context);
     }));
@@ -56,5 +88,5 @@ function initTasks(context) {
     loadTasks(context);
 }
 
-exports.activate = initTasks;
-exports.deactivate = deinitTasks;
+exports.activate = activate;
+exports.deactivate = deactivate;
