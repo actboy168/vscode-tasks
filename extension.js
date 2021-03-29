51674e20064fcc2a65ba9e67bead1147baed3290
tregota <spamrisk@gmail.com>
2021-03-29 14:38:18 +0200

    different label and color while task is running

--- extension.js
+++ extension.js
@@ -355,11 +355,12 @@ function matchTasks(taskInfo, taskMap, config) {
         if (hide) {
             continue;
         }
+        const running = vscode.tasks.taskExecutions.find(e => e._id === task._id) !== undefined;
         taskInfo.push({
             task: task,
-            label: getStatusBar(taskCfg, config, "label"),
+            label: (running && getStatusBar(taskCfg, config, "labelRunning")) || getStatusBar(taskCfg, config, "label"),
             tooltip: getStatusBar(taskCfg, config, "tooltip"),
-            color: getStatusBar(taskCfg, config, "color"),
+            color: (running && getStatusBar(taskCfg, config, "colorRunning")) || getStatusBar(taskCfg, config, "color"),
             backgroundColor: getStatusBar(taskCfg, config, "backgroundColor"),
             filePattern: getStatusBar(taskCfg, config, "filePattern"),
         });
